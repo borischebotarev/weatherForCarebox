7c2d1315976ec465012954a87cc5042b27f0b462
b.chebotarev <b.chebotarev@softteco.com>
2023-11-23 09:53:58 +0530

    update server

--- proxy-server.js
+++ proxy-server.js
@@ -2,12 +2,13 @@ const express = require('express');
 const axios = require('axios');
 const app = express();
 const PORT = 3000;
+let config = process.config.weather || require('./config.json');
 
 app.use(express.json());
 
 // CORS handling middleware
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Origin', config.domain);
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
@@ -15,8 +16,7 @@ app.use((req, res, next) => {
 // Route to access Tomorrow API
 app.get('/get-weather', async (req, res) => {
   try {
-    // TODO: move apikey to ENV configuration
-    const tomorrowAPIResponse = await axios.get(`https://api.tomorrow.io/v4/weather/forecast?location=new%20york&apikey=GafQV8qonJrDBZeG9LqGgltxmqZKy4pa`);
+    const tomorrowAPIResponse = await axios.get(`https://api.tomorrow.io/v4/weather/forecast?location=new%20york&apikey=` + config.tomorrowKey);
     res.json(tomorrowAPIResponse.data);
   } catch (error) {
     console.log(error)
